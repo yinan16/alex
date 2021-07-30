c270bb6e3492503a561ac7bb164f8074e5dfd739
Yinan Yu <yu.yinan16@gmail.com>
2021-07-30 20:09:55 +0200

    Update generated files

--- cache/cli_codegen_tf.py
+++ cache/cli_codegen_tf.py
@@ -62,59 +62,31 @@ def get_optimizer(trainable_params):
     optimizer_block_solver = tf.optimizers.Adam(learning_rate=optimizer_block_solver_decay_exponential_decay, beta_1=0.9, beta_2=0.999, epsilon=1e-08, name='optimizer_block/solver')
     return optimizer_block_solver 
 
+from alex.alex.checkpoint import Checkpoint
 
-from tensorflow import keras
-from tensorflow.keras import datasets
-import matplotlib.pyplot as plt
-import numpy as np
+C = Checkpoint("examples/configs/small1.yml",
+               None,
+               None)
 
-num_classes = 10
-(x_train, label_train), (x_test, label_test) = datasets.cifar10.load_data()
+ckpt = C.load()
 
-# Normalize pixel values to be between 0 and 1
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(label_train, num_classes)
-y_test = keras.utils.to_categorical(label_test, num_classes)
-
-
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(x_train[i])
-#     # The CIFAR labels happen to be arrays,
-#     # which is why you need the extra index
-#     plt.xlabel(class_names[label_train[i][0]])
-# plt.show()
-
-
-# from alex.alex.checkpoint import Checkpoint
-
-# C = Checkpoint("examples/configs/small1.yml",
-#                ["cache",  "config_1622420349826577.json"],
-#                ["checkpoints", None])
-
-# ckpt = C.load()
-ckpt = None
 trainable_variables = get_trainable_params(ckpt)
 
 from alex.alex import registry
 var_list = []
 for tv in trainable_variables:
-    if tv.split("/")[-1] in registry.ALL_TRAINABLE_PARAMS:
+    if tv.split('/')[-1] in registry.ALL_TRAINABLE_PARAMS:
         var_list.append(trainable_variables[tv])
 
-
 optimizer = get_optimizer(trainable_variables)
+
+
+def validation(inputs, labels):
+    preds = model(inputs, trainable_variables, training=False)
+    matches  = tf.equal(tf.math.argmax(preds,1), tf.math.argmax(labels,1))
+    accuracy = tf.reduce_mean(tf.cast(matches,tf.float32))
+    loss = tf.reduce_mean(get_loss(trainable_variables, [preds, labels]))
+    return accuracy, loss
 
 
 def train(x, gt, trainable_variables, var_list, optimizer):
@@ -123,37 +95,13 @@ def train(x, gt, trainable_variables, var_list, optimizer):
         gradients = tape.gradient(get_loss(trainable_variables, [gt, prediction]), var_list)
         optimizer.apply_gradients(zip(gradients, var_list))
 
-
-
-
-num_epochs = 90
-batch_size = 100
-
-train_loss_results = []
-train_accuracy_results = []
-
-train_ds = tf.data.Dataset.from_tensor_slices(
-    (x_train, y_train)).shuffle(50000).batch(batch_size)
-
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(10000)
-for x_test, y_test in test_ds:
-    break
-
-
-for epoch in range(num_epochs):
-    for i, (batch_x, batch_y) in enumerate(train_ds):
-      train(batch_x, batch_y, trainable_variables, var_list, optimizer)
-
-    preds = model(x_test, trainable_variables, training=False)
-
-    matches_test  = tf.equal(tf.math.argmax(preds,1), tf.math.argmax(y_test,1))
-
-    epoch_accuracy = tf.reduce_mean(tf.cast(matches_test,tf.float32))
-    current_loss = get_loss(trainable_variables, [preds, y_test])
-    epoch_loss_avg = tf.reduce_mean(current_loss)
-    train_loss_results.append(epoch_loss_avg)
-    train_accuracy_results.append(epoch_accuracy)
-
-    tf.print("[", epoch, "500]", "accuracy: ", epoch_accuracy, ", loss: ",epoch_loss_avg)
-
+def loop(trainloader, val_inputs, val_labels):
+    for epoch in range(90):
+        for i, (inputs, labels) in enumerate(trainloader):
+            train(inputs, labels, trainable_variables, var_list, optimizer)
+            if i % 500 == 499:
+                accuracy, loss = validation(val_inputs, val_labels)
+                
+                tf.print("[", epoch, "500]", "accuracy: ", accuracy, ", loss: ", loss)
+    print('Finished Training')
 
--- cache/new_mismatched_generated.py
+++ cache/new_mismatched_generated.py
@@ -112,16 +112,15 @@ class Model(torch.nn.Module):
         return optimizer_block_solver_decay_exponential_decay 
     
 
-import torch
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.ToTensor()])
+# ,
+#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
@@ -151,73 +150,10 @@ print(device)
 # show images
 # imshow(torchvision.utils.make_grid(images))
 # print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-test_images, test_labels = iter(testloader).next()
+test_inputs, test_labels = iter(testloader).next()
 
-def test(images, labels):
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        images = images.to(device)
-        labels = labels.to(device)
-
-        outputs = model(images, False)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-        # print('Accuracy of the network on the 10000 test images: %d %%' % (
-        #     100 * correct / total))
-    return correct / total
-
-
-# from alex.alex.checkpoint import Checkpoint
-
-# C = Checkpoint("examples/configs/small1.yml",
-#                ["cache",  "config_1622420349826577.json"],
-#                ["checkpoints", None])
-
-# ckpt = C.load()
-
-model = Model()
-
-model.to(device)
-
-optimizer = model.get_optimizer(model.params)
-
-learning_rate = model.get_scheduler(optimizer)
-
-
-for epoch in range(90):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-
-        inputs, labels = data
-
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        output = model(inputs, True)
-        loss = model.get_loss(model.trainable_params, [labels, output])
-        loss.backward()
-        optimizer.step()
-
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 500 == 499:    # print every 2000 mini-batches
-            accuracy = test(test_images, test_labels)
-            print('[%d, %5d] accuracy: %.3f, loss: %.3f' %
-                  (epoch + 1, i + 1, accuracy, running_loss / 500))
-            # C.save(model.trainable_params)
-            running_loss = 0.0
-    learning_rate.step()
-print('Finished Training')
+loop(trainloader, test_inputs, test_labels)
 
 
